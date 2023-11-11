693df9a9f49a9205df0af0e2f10f8082e1af3810
Paul-Particle <peter.ruschhaupt@gmail.com>
2023-11-11 17:10:05 +0100

    adjust variable names

--- src/scheduler.py
+++ src/scheduler.py
@@ -6,7 +6,8 @@ def calculate_next_quarter_hour():
 
 def schedule_tasks(tasks, start_time, time_limit):
     scheduled_tasks = []
-    current_time = start_time
+    # next time slot for scheduling
+    time_slot_start = start_time
     cumulative_short_duration = 0
 
     for calendar_key, task_description, duration in tasks:
@@ -15,33 +16,42 @@ def schedule_tasks(tasks, start_time, time_limit):
             cumulative_short_duration += duration
             if cumulative_short_duration >= 15:
                 # Add a gap and reset the counter
-                current_time += timedelta(minutes=15)
+                time_slot_start += timedelta(minutes=15)
                 cumulative_short_duration -= 15
             continue
 
         # Check if adding the current task exceeds the time limit
-        if current_time + timedelta(minutes=duration) > datetime.combine(datetime.today(), time_limit):
-            # Adjust end time to the time limit and schedule overlapping tasks
-            end_time = datetime.combine(datetime.today(), time_limit)
-            scheduled_tasks.append((calendar_key, task_description, duration, current_time, end_time))
+        if time_slot_start + timedelta(minutes=duration) > datetime.combine(datetime.today(), time_limit):
+            # Adjust task end time to the time limit 
+            time_slot_end = datetime.combine(datetime.today(), time_limit)
+            scheduled_tasks.append(
+                    (calendar_key, task_description, duration, time_slot_start, time_slot_end)
+                )
+            # schedule tasks overlapping from this point onwards
             continue
 
         # Schedule normal tasks
-        end_time = current_time + timedelta(minutes=duration)
-        scheduled_tasks.append((calendar_key, task_description, duration, current_time, end_time))
+        time_slot_end = time_slot_start + timedelta(minutes=duration)
+        scheduled_tasks.append((calendar_key, task_description, duration, time_slot_start, time_slot_end))
         
         # Update current time for the next task
-        current_time = end_time
+        time_slot_start = time_slot_end
 
     # Add buffer event if there's remaining time
-    if current_time.time() < time_limit:
+    if time_slot_start.time() < time_limit:
         buffer_end_time = datetime.combine(datetime.today(), time_limit)
-        scheduled_tasks.append(("buffer", "Buffer Time", (buffer_end_time - current_time).seconds // 60, current_time, buffer_end_time))
+        scheduled_tasks.append(
+                (
+                    "&", "Buffer Time", 
+                    (buffer_end_time - time_slot_start).seconds // 60, 
+                    time_slot_start, buffer_end_time
+                )
+            )
 
     return scheduled_tasks
 
 # Example usage
 time_limit = datetime.now().replace(hour=23, minute=0, second=0, microsecond=0).time()
-start_time = calculate_next_quarter_hour()  # This function needs to be implemented
+start_time = calculate_next_quarter_hour()
 
 scheduled_tasks = schedule_tasks(parsed_tasks, start_time, time_limit)
