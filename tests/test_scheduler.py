332a1e60f96bd01ddc2403ceb9983293db39fbd3
Paul-Particle <peter.ruschhaupt@gmail.com>
2023-11-11 17:43:11 +0100

    starting with unit testing

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,2 +1,19 @@
--e # Tests for scheduler
+import pytest
+from datetime import datetime, timedelta
+from scheduler import schedule_tasks, calculate_next_quarter_hour
 
+def test_simple_scheduling():
+    tasks = [("1", "Task 1", 30), ("2", "Task 2", 45)]
+    start_time = calculate_next_quarter_hour().time()
+    time_limit = datetime.now().replace(hour=23, minute=0, second=0, microsecond=0).time()
+
+    scheduled_tasks = schedule_tasks(tasks, start_time, time_limit)
+
+    assert len(scheduled_tasks) == 2
+    # ... additional assertions ...
+
+def test_short_tasks():
+    # Test scheduling of short tasks
+    pass
+
+# Additional test functions for other scenarios
