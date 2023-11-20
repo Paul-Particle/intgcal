2b8d2ccf40429e55a983d4ebaeb64e267cd35e61
Paul-Particle <peter.ruschhaupt@gmail.com>
2023-11-20 22:00:12 +0100

    refactor scheduler

--- src/intgcal/scheduler.py
+++ src/intgcal/scheduler.py
@@ -13,8 +13,8 @@ def calculate_next_quarter_hour(input_time=None):
     next_quarter = (input_time + timedelta(minutes=(15 - input_time.minute % 15)))
     return next_quarter.replace(second=0, microsecond=0)
 
-def schedule_tasks(tasks, start_time, time_limit):
-
+def add_date(start_time, end_time):
+    
     now = datetime.now()
     current_time = now.time()
     current_date = now.date()
@@ -25,9 +25,15 @@ def schedule_tasks(tasks, start_time, time_limit):
     if current_time > start_time.time():
         start_time = start_time + timedelta(days=1)
 
-    time_limit = datetime.combine(start_time.date(), time_limit)
+    end_time = datetime.combine(start_time.date(), end_time)
+    return start_time, end_time
+
+def schedule_tasks(tasks, start_time, end_time):
+
+    start_time, end_time = add_date(start_time, end_time)
 
     time_slot_start = start_time
+
     cumulative_short_duration = 0
     scheduled_tasks = []
 
@@ -35,24 +41,31 @@ def schedule_tasks(tasks, start_time, time_limit):
         # Handle short tasks for gap calculation
         if duration < 15:
             cumulative_short_duration += duration
-            # Align the next task to the nearest quarter hour if cumulative duration reaches 15 minutes
-            if cumulative_short_duration >= 15:
-                cumulative_short_duration = cumulative_short_duration % 15  # Keep the remainder
-                time_slot_start = (time_slot_start + timedelta(minutes=15)).replace(second=0, microsecond=0)
+            # Align the next task to the nearest quarter hour 
+        if cumulative_short_duration >= 15:
+            # Keep the remainder
+            cumulative_short_duration = cumulative_short_duration % 15
+            time_slot_start = (time_slot_start + timedelta(minutes=15)).replace(second=0, microsecond=0)
+        continue
 
-        else:
-            time_slot_end = time_slot_start + timedelta(minutes=duration)
-            if time_slot_end > time_limit:
-                # Adjust start time for overlapping tasks
-                time_slot_start = time_limit - timedelta(minutes=duration)
-                time_slot_end = time_limit
+        time_slot_end = time_slot_start + timedelta(minutes=duration)
+        if time_slot_end > end_time:
+            # Adjust start time to get overlapping tasks at end of day
+            time_slot_start = end_time - timedelta(minutes=duration)
+            time_slot_end = end_time
 
-            scheduled_tasks.append((calendar_key, task_description, duration, time_slot_start, time_slot_end))
-            time_slot_start = time_slot_end  # Update start time for the next task
+        scheduled_tasks.append(
+            (
+                calendar_key, task_description, duration,
+                time_slot_start, time_slot_end
+            )
+        )
+
+        time_slot_start = time_slot_end  # Update start time for the next task
 
     # Add buffer event if there's remaining time
-    if time_slot_start < time_limit:
-        buffer_end_time = time_limit
+    if time_slot_start < end_time:
+        buffer_end_time = end_time
 #        scheduled_tasks.append(
 #            ("&", "Buffer", 
 #             (buffer_end_time - time_slot_start).seconds // 60, time_slot_start, buffer_end_time)
