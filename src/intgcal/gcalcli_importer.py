e7a0b2973f5f89f62c14fe07f2b237616a1da58d
Paul-Particle <peter.ruschhaupt@gmail.com>
2023-11-28 11:50:12 +0100

    fixed importer

--- src/intgcal/gcalcli_importer.py
+++ src/intgcal/gcalcli_importer.py
@@ -8,8 +8,9 @@ def import_with_gcalcli(calendar_mapping, task_list_path):
     file_path_prefix = task_list_path.split('.txt')[0]
 
     for calendar_id in unique_calendar_ids:
-        ics_file = f'{file_path_prefix}_{calendar_id}.ics'
+        ics_file = f'{file_path_prefix} {calendar_id}.ics'
         if os.path.exists(ics_file):
             # Escape parentheses in calendar name (causes error for gcalcli)
             calendar_name = calendar_id.replace("(", r"\(").replace(")", r"\)")
-            subprocess.run(["gcalcli", "import", ics_file], "--calendar", calendar_name, check=True)
+            subprocess.run(["gcalcli", "import", ics_file, "--calendar",
+                            calendar_name], check=True)
